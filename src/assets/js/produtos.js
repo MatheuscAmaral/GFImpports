f931ea38e9ce973c02f5206605cc48e5c00eb295
Matheus Amaral <matheus.amaral.1443007@sga.pucminas.br>
2023-09-25 16:43:18 -0300

    mexendo na tela de login e nos cards ipads no mob

--- src/assets/js/produtos.js
+++ src/assets/js/produtos.js
@@ -1445,8 +1445,8 @@ const cardsPerPageMobile3 = 2; // Quantidade de cartões exibidos por página
 let currentPageMobile3 = 0;
 
 // Escolha os índices dos produtos que você deseja exibir
-const startProductIndex1Mobile3 = 0; // Índice do primeiro produto desejado
-const endProductIndex1Mobile3 = 36;   // Índice do último produto desejado (excluído)
+const startProductIndex1Mobile3 = 54; // Índice do primeiro produto desejado
+const endProductIndex1Mobile3 = 61;   // Índice do último produto desejado (excluído)
 
 function renderCardsMobile3() {
     cardsWrapperMobile3.innerHTML = "";
